d724a7545bb55762a9b2df1c22bebee4135e7f73
maenguin <maenguin924@gmail.com>
2021-03-08 21:08:37 +0900

    Conrefactor :  troller에 @RequestMapping 추가

--- src/main/java/maenguin/toymenuapi/api/MenuController.java
+++ src/main/java/maenguin/toymenuapi/api/MenuController.java
@@ -13,52 +13,54 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/store")
 public class MenuController {
 
     private final MenuService menuService;
 
-    @GetMapping("/api/v1/store/menu-group/menu/{menuId}")
+    @GetMapping("/menu-group/menu/{menuId}")
     public ApiResponse<MenuDto> findMenuById(@PathVariable("menuId") Long menuId) {
         return new ApiResponse<>(menuService.findMenuById(menuId));
     }
 
-    @GetMapping("/api/v1/store/{storeId}/menu-group")
+    @GetMapping("/{storeId}/menu-group")
     public ApiResponse<List<MenuGroupDto>> findAllMenuByStore(@PathVariable("storeId") Long storeId) {
         return ApiResponse.of(menuService.findAllByStore(storeId));
     }
 
-    @PostMapping("/api/v1/store/{storeId}/menu-group")
+    @PostMapping("/{storeId}/menu-group")
     public void saveMenuGroup(@PathVariable("storeId") Long storeId, @RequestBody MenuGroupSaveDto menuGroupSaveDto) {
         menuService.saveMenuGroup(storeId, menuGroupSaveDto);
     }
-    @PostMapping("/api/v1/store/menuGroup/{menuGroupId}/menu")
+    @PostMapping("/menuGroup/{menuGroupId}/menu")
     public void saveMenu(@PathVariable("menuGroupId") Long menuGroupId, @RequestBody MenuSaveDto menuSaveDto) {
         menuService.saveMenu(menuGroupId, menuSaveDto);
     }
 
-    @PutMapping("/api/v1/store/menuGroup/{menuGroupId}")
+    @PutMapping("/menuGroup/{menuGroupId}")
     public void updateMenuGroup(@PathVariable("menuGroupId") Long menuGroupId, @RequestBody MenuGroupSaveDto menuGroupSaveDto) {
         menuService.updateMenuGroup(menuGroupId, menuGroupSaveDto);
     }
-    @PutMapping("/api/v1/store/menuGroup/menu/{menuId}")
+    @PutMapping("/menuGroup/menu/{menuId}")
     public void updateMenu(@PathVariable("menuId") Long menuId,@RequestBody MenuSaveDto menuSaveDto) {
         menuService.updateMenu(menuId, menuSaveDto);
     }
 
 
 
-    @DeleteMapping("/api/v1/store/menuGroup/{menuGroupId}")
+    @DeleteMapping("/menuGroup/{menuGroupId}")
     public void deleteMenuGroup(@PathVariable("menuGroupId") Long menuGroupId) {
         menuService.deleteMenuGroup(menuGroupId);
     }
-    @DeleteMapping("/api/v1/store/menuGroup/menu/{menuId}")
+    @DeleteMapping("/menuGroup/menu/{menuId}")
     public void deleteMenu(@PathVariable("menuId") Long menuId) {
         menuService.deleteMenu(menuId);
     }
-    @DeleteMapping("/api/v1/store/menuGroup/menu/menuOptionGroup/{menuOptionGroupId}")
+    @DeleteMapping("/menuGroup/menu/menuOptionGroup/{menuOptionGroupId}")
     public void deleteMenuOptionGroup(@PathVariable("menuOptionGroupId") Long menuOptionGroupId) {
         menuService.deleteMenuGroup(menuOptionGroupId);
     }
+
 
 
 
